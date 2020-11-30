f01205b699aae51d6d60217d39e140b3923bbe7d
jenna <jenna@LAPTOP-81VP0EAC>
2020-11-30 15:41:00 +0200

    Lisätty Kayttaja-luokkaan metodien rungot

--- Verkkokauppa/src/app/Kayttaja.java
+++ Verkkokauppa/src/app/Kayttaja.java
@@ -4,7 +4,52 @@ public class Kayttaja {
 
 }
 
-class Asiakas {
+class Asiakas extends Kayttaja {
+	
+	//N‰ytt‰‰ yksitt‰isen tuotteen tarkemmat tiedot, esim. kuvauksen
+	protected void naytaTuotteenTiedot()
+	
+	{
+		
+	}
+	
+	//Lis‰‰ tuotteen ostoskoriin
+	protected void lisaaTuoteKoriin()
+
+	{
+		
+	}
+	
+	//Tulostaa ostoskorin yhteissumman
+	protected void naytaYhteissumma()
+	
+	{
+		
+	}
+}
+
+class Yllapito extends Asiakas {
 	
 	
+	//tulostaa varaston tiedot
+	private void tulostaVarasto()
+	
+	{
+		
+	}
+	
+	//muuttaa yksitt‰isen tuotteen hintaa
+	private void muutaHintaa()
+
+	{
+		
+	}
+	
+	
+	//Lis‰‰ yksitt‰isen tuotteen varastoon
+	private void lisaaTuote()
+	
+	{
+		
+	}
 }
