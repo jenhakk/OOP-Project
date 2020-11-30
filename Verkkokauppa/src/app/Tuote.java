613eb8e2f4d18393924435cc7ca3c60b09a60aef
jenna <jenna@LAPTOP-81VP0EAC>
2020-11-30 17:42:01 +0200

    Kayttaja-luokkaan: listat varastolle ja ostoskorille, attribuutit, oliot molemmista listoista, metodeihin alustavia toimintoja
    Tuote-luokkaan: attribuutit nimi, kuvaus, hinta, tuotenro(, kpl)
    Luokat Tuote ja Kori, niille konstruktorit sekä tulostaTiedot-metodi

--- Verkkokauppa/src/app/Tuote.java
+++ Verkkokauppa/src/app/Tuote.java
@@ -1,5 +1,64 @@
 package app;
 
 public class Tuote {
+	
+	
+	protected String nimi;
+	protected String kuvaus;
+	protected double hinta;
+	protected int tuotenro;
+	// private int kpl;
 
+	public Tuote()
+	{
+		nimi = "";
+		kuvaus = "";
+		hinta = 0;
+		tuotenro = 0;
+	}
+	
+	public Tuote(String n, String k, double h, int t) {
+		
+		nimi = n;
+		kuvaus = k;
+		hinta = h;
+		tuotenro = t;
+	}
+	
+	public void tulostaTiedot() {
+		
+		System.out.println("Nimi: " + nimi);
+		System.out.println("Kuvaus: " + kuvaus);
+		System.out.println("Hinta: " + hinta);
+		System.out.println("Tuote nro: " + tuotenro);
+	}
 }
+
+class Kori extends Tuote {
+	
+	public Kori()
+	{
+		nimi = "";
+		kuvaus = "";
+		hinta = 0;
+		tuotenro = 0;
+	}
+	
+	public Kori(String n, double h, int t) {
+		
+		nimi = n;
+		hinta = h;
+		tuotenro = t;
+	}
+}
+
+
+
+
+	
+	
+	
+	
+	
+	
+
