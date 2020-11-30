1514144b9668f343ba369c42ae041959d9f3b71f
Nuutti Jussila <nuutti.jussila@student.hamk.fi>
2020-11-30 21:21:03 +0200

    Revert "Lisätty uusi package "lib" ja sinne mySQL-connector-driver .jar -tiedosto tietokantayhteyttä varten."
    This reverts commit db779ba2b378f8cc464c44f04bf919aeb635daf9.

--- Verkkokauppa/src/app/Verkkokauppa.java
+++ Verkkokauppa/src/app/Verkkokauppa.java
@@ -1,11 +1,9 @@
 package app;
-import data.Tietokanta;
 
 public class Verkkokauppa {
 
 	public static void main(String[] args) {
-		// Tulostaa tuotteet-taulun sisällön - Testausta
-		Tietokanta.haeTuotteet();
+
 	}
 
 }
