8020e759c235be23c2e66ff1fed94b8774632673
Juho <Juho@DESKTOP-7AA967G>
2020-12-01 04:58:54 +0200

    Mainia tehty, kayttaja.java metodit private->protected

--- Verkkokauppa/src/app/Kayttaja.java
+++ Verkkokauppa/src/app/Kayttaja.java
@@ -1,7 +1,77 @@
 package app;
 
-public class Kayttaja {
-	
-	int nimi;
+import java.util.ArrayList;
 
+public class Kayttaja {
+
+	//Listat varastolle ja ostoskorille
+	
+	ArrayList<Tuote> tuotteet = new ArrayList<Tuote>();
+	ArrayList<Kori> ostoskori = new ArrayList<Kori>();
+	
+	private String nimi;
+	private String kuvaus;
+	private double hinta;
+	private int tuotenro;
+	// private int kpl;
+	
+	
+	Tuote tuote = new Tuote(nimi, kuvaus, hinta, tuotenro);
+	Kori ostos = new Kori(nimi, hinta, tuotenro);
+}
+
+class Asiakas extends Kayttaja {
+	
+	int i;
+	
+	//N‰ytt‰‰ yksitt‰isen tuotteen tarkemmat tiedot, esim. kuvauksen
+	protected void naytaTuotteenTiedot()
+	
+	{
+		tuotteet.get(i).tulostaTiedot();
+	}
+	
+	//Lis‰‰ tuotteen ostoskoriin
+	protected void lisaaTuoteKoriin()
+
+	{
+		ostoskori.add(ostos);
+	}
+	
+	//Tulostaa ostoskorin yhteissumman
+	protected void naytaYhteissumma()
+	
+	{
+		double summa;
+	}
+}
+
+class Yllapito extends Asiakas {
+	
+	
+	//tulostaa varaston tiedot
+	protected void tulostaVarasto()
+	
+	{
+		for (int i=0; i<tuotteet.size(); i++)
+			
+		{
+			System.out.println(tuotteet.get(i));
+		}
+	}
+	
+	//muuttaa yksitt‰isen tuotteen hintaa
+	protected void muutaHintaa()
+
+	{
+		
+	}
+	
+	
+	//Lis‰‰ yksitt‰isen tuotteen varastoon
+	protected void lisaaTuote()
+	
+	{
+		tuotteet.add(tuote);
+	}
 }
--- Verkkokauppa/src/app/Verkkokauppa.java
+++ Verkkokauppa/src/app/Verkkokauppa.java
@@ -1,10 +1,73 @@
 package app;
+import java.util.Scanner;
+
+import data.Tietokanta;
 
 public class Verkkokauppa {
 
 	public static void main(String[] args) {
-	int hinta; 
+		Tietokanta.haeTuotteet(); // testi
+		
+		Scanner input = new Scanner(System.in);
+		String vastaus;
+		
+		System.out.println("Tervetuloa Verkkokauppaan!");
+		System.out.println("Oletko asiakas vai ylläpito? (a/y)");
+		do {
+			vastaus = input.nextLine();
+			
+			if (!vastaus.equalsIgnoreCase("a") && !vastaus.equalsIgnoreCase("y")) {
+				System.out.println("virheellinen syöte, yritä uudelleen.");
+			}
+			
+		} while (!vastaus.equalsIgnoreCase("a") && !vastaus.equalsIgnoreCase("y"));
+		
+		//Asiakas valittu
+		if (vastaus.equalsIgnoreCase("a")) {
+			
+			Asiakas Asiakas = new Asiakas();
+			
+			Asiakas.naytaTuotteenTiedot();
+			Asiakas.lisaaTuoteKoriin();
+			Asiakas.naytaYhteissumma();
+			
+		}
+		
+		if (vastaus.equalsIgnoreCase("y")) {
+			
+			Yllapito Yllapito = new Yllapito();
+			
+			// kirjautuminen?
+			
+			System.out.println("Mitä haluaisit tehdä?");
+			System.out.println("Tulosta varasto: valitse (T)");
+			System.out.println("Muuttaa tuotteen hintaa: valitse (M)");
+			System.out.println("Lisätä tuotteen: valitse (L)");
+			
+			do {
+					vastaus = input.nextLine();
+					
+					if (vastaus.equalsIgnoreCase("t")) {
+						Yllapito.tulostaVarasto();
+					}
+					
+					if (vastaus.equalsIgnoreCase("m")) {
+						Yllapito.muutaHintaa();
+					}
+					
+					if (vastaus.equalsIgnoreCase("l")) {
+						Yllapito.lisaaTuote();
+					}
+					
+					if (!vastaus.equalsIgnoreCase("t") && !vastaus.equalsIgnoreCase("m") && !vastaus.equalsIgnoreCase("l")) {
+						System.out.println("virheellinen syöte, yritä uudelleen.");
+					}
+					
+				} while (!vastaus.equalsIgnoreCase("t") && !vastaus.equalsIgnoreCase("m") && !vastaus.equalsIgnoreCase("l"));
 
+			
+		}
+		
 	}
 
 }
